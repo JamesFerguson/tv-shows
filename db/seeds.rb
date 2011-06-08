dde558b730126ea80b2aec533310d9776a8395e5
JamesFerguson <james.ferguson7@gmail.com>
2011-06-09 08:17:08 +1000

    Seeds should do what it's meant to and just seed basic data, namely sources. Refactored to just take an array and do the creation in a loop, now checks first and only creates if necessary.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-Source.delete_all
-TvShow.delete_all
-Episode.delete_all
+sources = [
+  [
+    { :name => "Channel Seven" },
+    { :url => 'http://au.tv.yahoo.com/plus7/browse/', :scraper => "SevenScraper" }
+  ],
+  [
+    { :name => "Channel Nine" },
+    { :url => 'http://fixplay.ninemsn.com.au/catalogue', :scraper => "NineScraper" }
+  ]
+]
 
-require 'scrapers/seven_scraper'
-
-@source = Source.create(
-  :name => "Seven.com.au",
-  :url => 'http://au.tv.yahoo.com/plus7/browse/'
-)
-SevenScraper.extract_shows.each { |show_data| @source.tv_shows.create!(show_data) }
-@source.save!
-@source.tv_shows.each do |show|
-  SevenScraper.extract_show(show).each { |ep_data| show.episodes.create!(ep_data) }
-  show.save!
+sources.each do |source_data|
+  source = Source.where(source_data.first).first || Source.create!(source_data.first)
+  
+  source.update_attributes!(source_data.last)
 end
