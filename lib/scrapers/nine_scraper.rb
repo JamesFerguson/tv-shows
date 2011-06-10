acb27ff4081b20a4493c8e75500d91bd8017936d
JamesFerguson <james.ferguson7@gmail.com>
2011-06-10 23:05:51 +1000

    Fixed bad selector for getting shows that also picked up some random junk.

--- lib/scrapers/nine_scraper.rb
+++ lib/scrapers/nine_scraper.rb
@@ -4,7 +4,7 @@ class NineScraper
     
     page = Nokogiri::HTML(shows_url.open)
     
-    shows = page.css("a.linkItem").map do |node|
+    shows = page.css("#main a.linkItem").map do |node|
       {:name => node.text, :url => shows_url.merge(node.attributes['href'].value).to_s}
     end
   end
