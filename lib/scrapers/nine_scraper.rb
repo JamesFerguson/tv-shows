bd5d3c11be67e408739755a87e77519ee47f28b2
JamesFerguson <james.ferguson7@gmail.com>
2011-06-12 19:22:44 +1000

    refactor confusing var name

--- lib/scrapers/nine_scraper.rb
+++ lib/scrapers/nine_scraper.rb
@@ -12,7 +12,7 @@ class NineScraper
   def self.extract_episodes(show)
     page = Nokogiri::HTML(open(show.url))
     
-    shows = page.css("#all_articles_index .tr").map do |node|
+    episodes = page.css("#all_articles_index .tr").map do |node|
       {
         :name => "Episode #{node.children[1].text}: #{node.css('.season_title').text}",
         :url => URI.parse(show.url).
