7a6c96687f80b01c18b76a7ab073a74d66b4befc
JamesFerguson <james.ferguson7@gmail.com>
2011-06-15 07:48:37 +1000

    New source spec requires cleanup based on new field deactivated_at, new scope active, shows and episodes only deleted after 1 month's inactivity.

--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -34,39 +34,6 @@ describe Source do
       @source.tv_shows.where(:id => @b.id).first.should be_nil
       Episode.all.first.should be_nil
     end
-    
-    # before(:each) do
-    #   show_hashes = {}
-    #   %w{A B C}.each do |show|
-    #     show_hashes[show.to_sym] = { :name => show, :url => "http://" + show }
-    #   end
-    #   @source = Source.last
-    #   @a = @source.tv_shows.make(show_hashes[:A])
-    #   @a1 = @a.episodes.make(:url => "http://a1")
-    #   @b = @source.tv_shows.make(show_hashes[:B])
-    #   @b1 = @b.episodes.make(:url => "http://b1")
-    #   @b2 = @b.episodes.make(:url => "http://b2")
-    # end
-    # 
-    # it "works!" do
-    #   @source.scraper_class.stub(:extract_shows).
-    #     and_return([show_hashes[:B], show_hashes[:C]])
-    #   @source.scraper_class.stub(:extract_shows).with(@b)
-    #     and_return()
-    # end
-    # create an old tv show, A with an episode A1
-    # create a tv show, B, with an old episode, B1, and an episode B2
-    # stub extract_shows to return B and C
-    # stub extract_episodes(B) to return B2
-    # stub extract_episodes(C) to return C1
-    
-    # source.scrape
-    
-    # A should not exist
-    # A1 should not exist
-    # B1 should not exist
-    # B & C should exist
-    # B2 & C1 should exist
   end
   
   context "Source.find_or_create" do
@@ -98,11 +65,14 @@ describe Source do
     source = Source.make
     source.tv_shows << TvShow.make(:name => "Number 1")
     source.tv_shows << TvShow.make
-    source.tv_shows.last.update_attributes!(:updated_at => DateTime.now - 3.hours)
+    source.tv_shows.last.update_attributes!(:deactivated_at => DateTime.now - 3.hours)
+    source.tv_shows << TvShow.make
+    source.tv_shows.last.update_attributes!(:deactivated_at => DateTime.now - 1.month)
 
     source.send(:cleanup, source.tv_shows)
 
-    source.tv_shows.count.should == 1
-    source.tv_shows.first.name.should == "Number 1"
+    source.tv_shows.count.should == 2
+    source.tv_shows.active.count.should == 1
+    source.tv_shows.active.last.name.should == "Number 1"
   end
 end
