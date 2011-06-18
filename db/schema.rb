95183ceabbd8e37fd1a6d2fc64c18ac65abe566d
JamesFerguson <james.ferguson7@gmail.com>
2011-06-18 18:32:52 +1000

    Add slug to tv_show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110614220557) do
+ActiveRecord::Schema.define(:version => 20110616222952) do
 
   create_table "episodes", :force => true do |t|
     t.string   "name"
@@ -23,13 +23,28 @@ ActiveRecord::Schema.define(:version => 20110614220557) do
     t.datetime "deactivated_at"
   end
 
+  create_table "slugs", :force => true do |t|
+    t.string   "name"
+    t.integer  "sluggable_id"
+    t.integer  "sequence",                     :default => 1, :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
+  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
+
   create_table "sources", :force => true do |t|
     t.string   "name"
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "scraper"
+    t.string   "cached_slug"
   end
+
+  add_index "sources", ["cached_slug"], :name => "index_sources_on_cached_slug", :unique => true
 
   create_table "tv_shows", :force => true do |t|
     t.string   "name"
@@ -38,6 +53,9 @@ ActiveRecord::Schema.define(:version => 20110614220557) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deactivated_at"
+    t.string   "cached_slug"
   end
+
+  add_index "tv_shows", ["source_id", "cached_slug"], :name => "index_tv_shows_on_source_id_and_cached_slug", :unique => true
 
 end
