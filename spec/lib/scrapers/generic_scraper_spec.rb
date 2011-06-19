ca55cb93a1b4a226ef0a91c867a978435395e2f4
JamesFerguson <james.ferguson7@gmail.com>
2011-06-20 08:19:14 +1000

    Messy but it passes.

--- spec/lib/scrapers/generic_scraper_spec.rb
+++ spec/lib/scrapers/generic_scraper_spec.rb
@@ -18,28 +18,24 @@ Source.where(:name => "SMH.tv").first.tv_shows.make(
 describe "any scraper" do
   before do
     FakeWeb.allow_net_connect = false
-    @sources = Source.all
   end
   
   context "after faking scrapers' source urls" do
     before do
-      @sources.each do |source|
+      Source.all.each do |source|
         require "scrapers/#{source.scraper.underscore}.rb"
-        
-        source_url = source.url.gsub(%r{/}, '^')
-        source_url = (source_url !~ /(\.html?|\.xml)$/ ? source_url + '.htm' : source_url)
-        FakeWeb.register_uri(
-          :get, 
-          source.url, 
-          :response => File.read(Rails.root + 
-                                  "spec/fakeweb/pages/#{source_url}")
-        )
       end
     end
 
     Source.all.each do |source|
       it "scrapes the index for #{source.name} ok" do
-        source.scraper_class.extract_shows(source.url).map(&:stringify_keys).should == 
+        source_url = source.url.gsub(%r{/}, '^')
+        source_url = (source_url !~ /(\.html?|\.xml)$/ ? source_url + '.htm' : source_url)
+        source.scraper.constantize.should_receive(:read_url).with(URI.parse(source.url)).and_return(
+            File.read(Rails.root + "spec/fakeweb/pages/#{source_url}")
+        )
+
+        source.scraper_class.extract_shows(source.url).map(&:stringify_keys).should ==
           JSON.parse(File.read(
             "spec/fakeweb/results/#{source.url.gsub(%r{/}, '^')}.json"
           ))        
@@ -47,8 +43,15 @@ describe "any scraper" do
     end
 
     it "excludes slideshow, poll, etc when parsing channel nine" do
+      source = Source.where(:name => "Channel Nine").first
+      source_url = source.url.gsub(%r{/}, '^')
+      source_url = (source_url !~ /(\.html?|\.xml)$/ ? source_url + '.htm' : source_url)
+      NineScraper.should_receive(:read_url).with(URI.parse(source.url)).and_return(
+          File.read(Rails.root + "spec/fakeweb/pages/#{source_url}")
+      )
+
       NineScraper.extract_shows(
-                                Source.where(:name => "Channel Nine").first.url
+        source.url
       ).map do |show_data|
         URI.parse(show_data[:url]).host
       end.
@@ -63,11 +66,8 @@ describe "any scraper" do
 
         show_url = show.url.gsub(%r{/}, '^')
         show_url = (show_url !~ /(\.html?|\.xml)$/ ? show_url + '.htm' : show_url)
-        FakeWeb.register_uri(
-          :get, 
-          show.url, 
-          :response => File.read(Rails.root + 
-                                  "spec/fakeweb/pages/#{show_url}")
+        show.source.scraper_class.stub(:read_url).and_return(
+            File.read(Rails.root + "spec/fakeweb/pages/#{show_url}")
         )
       end
     end
