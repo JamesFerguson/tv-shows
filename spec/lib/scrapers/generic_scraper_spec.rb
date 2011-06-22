db929677b1cf1ee6cd6a67a50d1181ff92eaf31d
JamesFerguson <james.ferguson7@gmail.com>
2011-06-23 05:12:23 +1000

    Refactor url mangling out into a method. Always add .htm extension now even when producing name of a json file. Rename json files accordingly.

--- spec/lib/scrapers/generic_scraper_spec.rb
+++ spec/lib/scrapers/generic_scraper_spec.rb
@@ -7,7 +7,7 @@ end
 TvShow.destroy_all # rspec not deleting old records for some reason.
 seed_tv_shows # Done here so we can do TvShow.all.each { |show| it "does something" do ... end }
 
-describe "any scraper" do
+describe "the scraper in question" do
   before do
     FakeWeb.allow_net_connect = false
   end
@@ -15,25 +15,21 @@ describe "any scraper" do
   context "after faking scrapers' source urls" do
     Source.all.each do |source|
       it "scrapes the index for #{source.name} ok" do
-        source_url = source.url.gsub(%r{/}, '^')
-        source_url = (source_url !~ /(\.html?|\.xml)$/ ? source_url + '.htm' : source_url)
         source.scraper.constantize.should_receive(:read_url).with(URI.parse(source.url)).and_return(
-            File.read(Rails.root + "spec/fakeweb/pages/#{source_url}")
+            File.read(Rails.root + "spec/fakeweb/pages/#{fakewebize(source.url)}")
         )
 
         source.scraper_class.extract_shows(source.url).map(&:stringify_keys).should ==
           JSON.parse(File.read(
-            "spec/fakeweb/results/#{source.url.gsub(%r{/}, '^')}.json"
+            "spec/fakeweb/results/#{fakewebize(source.url)}.json"
           ))        
       end
     end
 
     it "excludes slideshow, poll, etc when parsing channel nine" do
       source = Source.where(:name => "Channel Nine").first
-      source_url = source.url.gsub(%r{/}, '^')
-      source_url = (source_url !~ /(\.html?|\.xml)$/ ? source_url + '.htm' : source_url)
       NineScraper.should_receive(:read_url).with(URI.parse(source.url)).and_return(
-          File.read(Rails.root + "spec/fakeweb/pages/#{source_url}")
+          File.read(Rails.root + "spec/fakeweb/pages/#{fakewebize(source.url)}")
       )
 
       NineScraper.extract_shows(
@@ -46,25 +42,22 @@ describe "any scraper" do
   end
   
   context "after populating tv_shows and faking their urls" do
-    before(:each) do
-      TvShow.all.each do |show|
-        require "scrapers/#{show.source.scraper.underscore}.rb"
-
-        show_url = show.url.gsub(%r{/}, '^')
-        show_url = (show_url !~ /(\.html?|\.xml)$/ ? show_url + '.htm' : show_url)
-        show.source.scraper_class.stub(:read_url).and_return(
-            File.read(Rails.root + "spec/fakeweb/pages/#{show_url}")
-        )
-      end
-    end
-
     TvShow.all.each do |show|
       it "scrapes the episodes for #{show.name} ok" do
-        show.source.scraper_class.extract_episodes(show).map(&:stringify_keys).should == 
+        show.source.scraper_class.should_receive(:read_url).and_return(
+            File.read(Rails.root + "spec/fakeweb/pages/#{fakewebize(show.url)}")
+        )
+
+        show.source.scraper_class.extract_episodes(show).map(&:stringify_keys).should ==
           JSON.parse(File.read(
-            "spec/fakeweb/results/#{show.url.gsub(%r{/}, '^')}.json"
+            "spec/fakeweb/results/#{fakewebize(show.url)}.json"
           ))
       end
     end
+  end
+
+  def fakewebize(url)
+    url = url.gsub(%r{/}, '^')
+    url = (url !~ /(\.html?|\.xml)$/ ? url + '.htm' : url)
   end
 end
