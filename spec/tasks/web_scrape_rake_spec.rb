a5fc62137f872964a814969ce451f0fb66334268
JamesFerguson <james.ferguson7@gmail.com>
2011-06-24 03:28:32 +1000

    Add an integration spec of the web:scrape* tasks. Expects web:scrape_shows and web:scrape_episodes instead of the current web:scrape (for easy mocking).

---
+++ spec/tasks/web_scrape_rake_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+require 'rake'
+
+Source.all.each do |source|
+  require "scrapers/#{source.scraper.underscore}.rb"
+end
+
+TvShow.destroy_all # rspec not deleting old records for some reason.
+
+describe "rake web:scrape" do
+  include FakewebHelper
+
+  before do
+    FakeWeb.allow_net_connect = false
+
+    @rake = Rake.application
+    @rake.init
+    @rake.load_rakefile
+  end
+
+  context "after faking pages for all source urls" do
+    before(:each) do
+      Source.all.each do |source|
+        show_urls = source.scraper_class.extract_show_urls(source.url)
+
+        show_urls.each.with_index do |url, index|
+          expectation = source.scraper_class.should_receive(:read_url).with(URI.parse(url))
+
+          expectation.twice if index == 0 && show_urls.count > 1 # pagination double scrapes first page
+
+          expectation.and_return(
+              File.read(Rails.root + "spec/fakeweb/pages/#{fakewebize(url)}")
+          )
+        end
+      end
+    end
+
+    it "should create some shows" do
+      puts "invoking scrape_shows"
+      @rake["web:scrape_shows"].invoke
+
+      Source.where(:name => "Channel Nine").first.tv_shows.count.should == 38
+      Source.where(:name => "Channel Seven").first.tv_shows.count.should == 60
+      Source.where(:name => "ABC 1").first.tv_shows.count.should == 55
+      Source.where(:name => "ABC 2").first.tv_shows.count.should == 36
+      Source.where(:name => "ABC 3").first.tv_shows.count.should == 36
+      Source.where(:name => "iView Originals").first.tv_shows.count.should == 8
+      Source.where(:name => "SMH.tv").first.tv_shows.count.should == 174
+
+      Source.count.should == 7
+    end
+  end
+end
