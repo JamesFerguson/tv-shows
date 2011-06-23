44a2aa993b91c37c550383567ef0322bafcb467c
JamesFerguson <james.ferguson7@gmail.com>
2011-06-24 03:35:15 +1000

    slight refactoring

--- spec/tasks/db_seed_rake_spec.rb
+++ spec/tasks/db_seed_rake_spec.rb
@@ -3,13 +3,14 @@ require 'rake'
 
 describe "rake db:seed" do
   before do
-    rake = Rake.application
-    rake.init
-    rake.load_rakefile
-    rake["db:seed"].invoke
+    @rake = Rake.application
+    @rake.init
+    @rake.load_rakefile
   end
   
   it "should create some sources" do
+    @rake["db:seed"].invoke
+
     Source.count.should > 0
   end
 end
