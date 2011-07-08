597c8d00f6f1f5c532c0f5c89850ac5c0aba0889
JamesFerguson <james.ferguson7@gmail.com>
2011-07-08 23:29:01 +1000

    Early pass at an atom feed mapping. Tried to handle multiple links within Feed with a separate class but (at least with nunemaker's branch) it returned every link in the xml, not just immediate children, even with :deep => false.

---
+++ spec/support/atom_feed_mapping.rb
@@ -0,0 +1,41 @@
+module AtomFeedMapping
+  class Link
+    include HappyMapper
+    tag 'link'
+
+    attribute :rel, String
+    attribute :type, String
+    attribute :href, String
+  end
+
+  class Entry
+    include HappyMapper
+    tag 'entry'
+
+    element :id, String, :single => true
+    element :published, DateTime, :single => true
+    element :updated, DateTime, :single => true
+    element :title, String, :single => true
+    element :content, String, :single => true
+    element :author_name, String, :single => true, :tag => 'name', :deep => true
+    has_one :link, Link, :deep => false
+  end
+
+  class Feed
+    include HappyMapper
+    tag 'feed'
+
+    attribute :xmlns, String, :single => true
+    element :id, String, :single => true
+    element :title, String, :single => true
+    element :updated, DateTime, :single => true
+#    has_many :links, Link, :deep => false
+    element :link, XmlContent, :single => false, :deep => false, :attributes => {
+        :rel => String,
+        :type => String,
+        :href => String
+      }
+    has_many :entries, Entry
+  end
+end
+
