3e1b683bc9e6010c25b904b3fe5baab80b9687ed
JamesFerguson <james.ferguson7@gmail.com>
2011-07-14 20:38:25 +1000

    be on the safe side

--- lib/scrapers/nine_scraper.rb
+++ lib/scrapers/nine_scraper.rb
@@ -22,7 +22,7 @@ class NineScraper < BaseScraper
                   merge(
                     node.css('.watch_now_normal').first.attributes['href'].value
                   ).to_s,
-        :ordering => 100 - index
+        :ordering => 1000 - index
       }
     end
   end
