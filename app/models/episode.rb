7de8af9275ae5590d3fcf144b78e3b0a8efaaa49
JamesFerguson <james.ferguson7@gmail.com>
2011-07-14 18:04:35 +1000

    Validate unique ordering per tv_show_id, also make it required to ensure tests cover everything.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,6 +1,9 @@
 class Episode < ActiveRecord::Base
   belongs_to :tv_show
 
+  validates_presence_of :ordering
+  validates_uniqueness_of :ordering, :scope => :tv_show_id
+
   scope :active, where(:deactivated_at => nil)
   
 end
