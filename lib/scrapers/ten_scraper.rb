06bff6efd27b0dfe014403f9d3e7e3f93601e94a
JamesFerguson <james.ferguson7@gmail.com>
2011-08-03 07:33:53 +1000

    First attempts at a TenScraper.

--- lib/scrapers/ten_scraper.rb
+++ lib/scrapers/ten_scraper.rb
@@ -1,8 +1,14 @@
-class TenScraper
-  SHOWS_URL = URI.parse('http://ten.com.au/tvshows.htm')
-  
-  def self.extract_shows
-    page = Nokogiri::HTML(SHOWS_URL.open)
+require Rails.root + 'lib/scrapers/base_scraper'
+
+class TenScraper < BaseScraper
+  def extract_show_urls(source_url)
+    token = read_url(source_url).sub(%r{.*<token>(.*)</token>.*}, '\1')
+
+    ["http://api.v2.movideo.com/rest/playlist/41398?depth=1&token=#{token}&mediaLimit=50&includeEmptyPlaylists=false&omitFields=client,mediaSchedules,cuePointsExist,encodingProfiles,mediaType,ratio,syndicated,tagProfileId"]
+  end
+
+  def self.extract_shows(source_url)
+    page = Nokogiri::HTML(source_url)
     
     shows = page.css("li.item a").map do |node|
       [node.text, SHOWS_URL.merge(node.attributes['href'].value).to_s]
