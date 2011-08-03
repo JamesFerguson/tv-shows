a68e4f6484e2b4af8a5d1b15b8fef360ac410c1a
JamesFerguson <james.ferguson7@gmail.com>
2011-08-03 17:49:22 +1000

    Give full url, we'll truncate it

--- lib/scrapers/ten_scraper.rb
+++ lib/scrapers/ten_scraper.rb
@@ -4,7 +4,7 @@ class TenScraper < BaseScraper
   def extract_show_urls(source_url)
     token = read_url(source_url).sub(%r{.*<token>(.*)</token>.*}, '\1')
 
-    ["http://api.v2.movideo.com/rest/playlist/41398?depth=1&token=#{token}&mediaLimit=50&includeEmptyPlaylists=false&omitFields=client,mediaSchedules,cuePointsExist,encodingProfiles,mediaType,ratio,syndicated,tagProfileId"]
+    ["http://api.v2.movideo.com/rest/playlist/41398?depth=1&token=#{token}&mediaLimit=50&includeEmptyPlaylists=false&omitFields=client,copyright,mediaSchedules,cuePointsExist,encodingProfiles,filename,imageFilename,mediaFileExists,mediaType,ratio,status,syndicated,tagProfileId,advertisingConfig,tagOptions,podcastSupported,syndicatedPartners,creationDate,lastModifiedDate"]
   end
 
   def self.extract_shows(source_url)
