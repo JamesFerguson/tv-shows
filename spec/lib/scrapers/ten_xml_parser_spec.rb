0444cd2cf1bd088f11f7ceba7a5dd308441c59a6
JamesFerguson <james.ferguson7@gmail.com>
2011-09-09 07:48:25 +1000

    Spec ten_xml_parser - so far just speccing out having a list of all external links in a feed

---
+++ spec/lib/scrapers/ten_xml_parser_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require Rails.root.join 'lib/scrapers/ten_xml_parser'
+
+describe TenXmlParser do
+  include FakewebHelper
+
+  before(:each) do
+    FakeWeb.allow_net_connect = false
+
+    @ten_url = "http://api.v2.movideo.com/rest/playlist/41398?depth=4&token=abc&mediaLimit=50&includeEmptyPlaylists=false&omitFields=client,copyright,mediaSchedules,cuePointsExist,encodingProfiles,filename,imageFilename,mediaFileExists,mediaType,ratio,status,syndicated,tagProfileId,advertisingConfig,tagOptions,podcastSupported,syndicatedPartners,creationDate,lastModifiedDate,isAdvertisement,defaultImage,imagePath"
+    @ten_xml = File.read(Rails.root.join("spec/fakeweb/pages/#{fakewebize(@ten_url)}"))
+
+    @playlist = TenXmlParser::Playlist.parse(@ten_xml)
+  end
+
+  it "finds external links" do
+    @playlist.external_links.size.should == 19
+    @playlist.external_links.select { |link| link =~ /one.com.au|eleven.com.au/ }.should be_any
+  end
+end
