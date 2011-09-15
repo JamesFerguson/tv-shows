389d6c7fa8d1612fce4923b84e7c88ec88af3a51
JamesFerguson <james.ferguson7@gmail.com>
2011-09-15 20:10:27 +1000

    Fix to regex to handle movideo url being urlencoded.

--- lib/scrapers/ten_scraper.rb
+++ lib/scrapers/ten_scraper.rb
@@ -18,16 +18,15 @@ class TenScraper < BaseScraper
   def self.extract_show_urls(source_url)
     @@token = read_url(source_url).sub(/.*<token>(.*)<\/token>.*/m, '\1')
 
-    playlist_id = PLAYLIST_IDS[source_url.scan(/key=movideo([^&]+)/).flatten.first]
-
+    playlist_id = PLAYLIST_IDS[source_url.scan(/key(?:=|%3D)movideo([^&%]+)/).flatten.first]
     ["http://api.v2.movideo.com/rest/playlist/#{playlist_id}?depth=4&token=#{@@token}&mediaLimit=50&includeEmptyPlaylists=false&omitFields=client,copyright,mediaSchedules,cuePointsExist,encodingProfiles,filename,imageFilename,mediaFileExists,mediaType,ratio,status,syndicated,tagProfileId,advertisingConfig,tagOptions,podcastSupported,syndicatedPartners,creationDate,lastModifiedDate,isAdvertisement,defaultImage,imagePath"]
   end
 
   def self.extract_shows(source_url)
     playlist = TenXmlParser::Playlist.parse(read_url(source_url))
-puts "url: #{source_url}"
-puts "source: #{read_url(source_url)}"
-puts "playlist: #{playlist.inspect}"
+# puts "url: #{source_url}"
+# puts "source: #{read_url(source_url)}"
+# puts "playlist: #{playlist.inspect}"
     unknown_external_links = filter_links(playlist.external_links)
     if unknown_external_links.any? && !ENV['IGNORE_EXT_LINKS']
       Rails.logger.info "Unknown external links found:\n#{unknown_external_links.inspect}"
@@ -45,17 +44,17 @@ puts "playlist: #{playlist.inspect}"
   end
 
   PLAY_URLS = {
-    "Ten" => "http://ten.com.au/watch-tv-episodes-online.htm",
-    "OneHd" => "http://one.com.au/video.htm",
-    "Eleven" => "http://eleven.com.au/catch-up-and-videos.htm",
-    "Neighbours" => "http://neighbours.com.au/video.htm?movideo_p=41267" #,
-    # "Masterchef" => "http://www.masterchef.com.au/video.htm" #,
-    # "Ready Steady Cook" => "http://readysteadycook.ten.com.au/video.htm" #,
-    # "Stargate Universe" => "http://www.stargate-universe.com.au/full-episodes/" #,
-    # "The Biggest Loser" => "http://thebiggestloser.com.au/video.htm?" #,
-    # "The Circle" => "http://ten.com.au/the-circle-video.htm?" #,
-    # "The Renovators" => "http://therenovatorstv.com.au/video.htm?" #,
-    # "The 7PM Project" => "http://7pmproject.com.au/video.htm"
+    "Ten"               => "http://ten.com.au/watch-tv-episodes-online.htm",
+    "OneHd"             => "http://one.com.au/video.htm",
+    "Eleven"            => "http://eleven.com.au/catch-up-and-videos.htm",
+    "Neighbours"        => "http://neighbours.com.au/video.htm?movideo_p=41267",
+    "Masterchef"        => "http://www.masterchef.com.au/video.htm",
+    "Ready Steady Cook" => "http://readysteadycook.ten.com.au/video.htm",
+    "Stargate Universe" => "http://www.stargate-universe.com.au/full-episodes/",
+    "The Biggest Loser" => "http://thebiggestloser.com.au/video.htm?",
+    "The Circle"        => "http://ten.com.au/the-circle-video.htm?",
+    "The Renovators"    => "http://therenovatorstv.com.au/video.htm?",
+    "The 7PM Project"   => "http://7pmproject.com.au/video.htm"
   }
   def self.extract_episodes(show)
     page = TenXmlParser::MediaList.parse(read_url(show.url)).first
