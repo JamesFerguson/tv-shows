0a78941db6ffe816a376c2dfefb8cbe0a50d8d11
JamesFerguson <james.ferguson7@gmail.com>
2011-09-15 20:08:47 +1000

    Get the local tunnel to a workable state

--- lib/local_tunnel/sync_to_server.sh
+++ lib/local_tunnel/sync_to_server.sh
@@ -2,9 +2,8 @@
 /usr/bin/ftp ftp.users.on.net << FTP_CMDS
    prompt
    lcd "~/Coding/tv-shows/lib/local_tunnel/"
-   put "index.html"
    put "index.php"
-   put ""index.pl"
+   chmod 755 index.php
    quit
 FTP_CMDS
 
