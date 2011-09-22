f5f1bd831c6e0ed6934aa695eea80c8e8e171bc9
JamesFerguson <james.ferguson7@gmail.com>
2011-09-23 07:35:59 +1000

    Add description, duration and image to episodes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110919082148) do
+ActiveRecord::Schema.define(:version => 20110922212842) do
 
   create_table "episodes", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(:version => 20110919082148) do
     t.datetime "updated_at"
     t.datetime "deactivated_at"
     t.integer  "ordering"
+    t.text     "description"
+    t.integer  "duration"
+    t.text     "image"
   end
 
   add_index "episodes", ["tv_show_id", "ordering"], :name => "index_episodes_on_tv_show_id_and_ordering", :unique => true
