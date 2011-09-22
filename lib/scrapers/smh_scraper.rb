889e2f66b3b03a46b10b19e944c72a1ec736731c
JamesFerguson <james.ferguson7@gmail.com>
2011-09-22 21:40:19 +1000

    Capture each show's image, genre and description.

--- lib/scrapers/smh_scraper.rb
+++ lib/scrapers/smh_scraper.rb
@@ -6,19 +6,28 @@ class SmhScraper < BaseScraper
 
     show_urls = page.css("li.page a").map { |node| node.attributes['href'].value }.unshift(source_url)
   end
-  
+
   def self.extract_shows(source_url)
     page = Nokogiri::HTML(read_url(source_url))
     source_url = URI.parse(source_url)
 
     shows = page.css("ul.cN-listStoryTV h5 a").map do |node|
-      {:name => node.text, :url => source_url.merge(node.attributes['href'].value).to_s}
+      {
+        :name => node.text,
+        :url => source_url.merge(node.attributes['href'].value).to_s
+      }
     end
   end
-  
+
   def self.extract_episodes(show)
     show_url = URI.parse(show.url)
     page = Nokogiri::HTML(read_url(show.url))
+
+    show.update_attributes!(
+      :image => page.css('div.wrapShow img').first['src'],
+      :genre => (page.css('div.wof p.more a').first || page.css('div.cS-rateMetadata dd a')[1]).text,
+      :description => (page.css('div.wof p').last || page.css('div.cS-rateMetadata dd')[2]).text
+    )
 
     episodes = page.css("ul.cN-listStoryTV").first.css('li').reverse.map.with_index do |node, index|
       link = node.css('h5 a').first
