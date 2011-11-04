44d1d7331342d38bdabd2e9974a238dc650aadf9
JamesFerguson <james.ferguson7@gmail.com>
2011-11-04 14:25:04 +1100

    Move all helper methods into FakeWebHelper, added a number of new methods.

--- spec/support/fakeweb_helper.rb
+++ spec/support/fakeweb_helper.rb
@@ -1,14 +1,56 @@
 module FakewebHelper
-  def fakewebize(url)
-    url = url.gsub(%r{/}, '^').gsub(%r{&token=[^&]+|&omitFields=[^&]+}, '')
-    url = (url !~ /(\.html?|\.xml)$/ ? url + '.htm' : url)
+  def download_page_if_new(source, url)
+    if first_scrape?(source)
+      puts "First Scrape: #{url}"
+
+      download_page(url)
+    end
   end
 
   def first_scrape?(source)
-    ((ENV['FIRST_SCRAPE'] || '').split(',') & [source.name, source.scraper]).any?
+    ((ENV['FIRST_SCRAPE'] || '').split(',') & [source.name, source.scraper, 'All']).any?
+  end
+
+  def download_page(url)
+    `curl --silent -L #{Shellwords.shellescape(url)} >#{Shellwords.shellescape((Rails.root + "spec/fakeweb/pages/#{fakewebize(url)}").to_s)}`
+  end
+
+
+  def prefill_results_if_new(source, url, results, ext = '.json')
+    if first_results?(source)
+      puts "First Results: #{url}"
+
+      prefill_results(source, url, results, ext)
+    end
   end
 
   def first_results?(source)
-    ((ENV['FIRST_RESULTS'] || '').split(',') & [source.name, source.scraper]).any?
+    ((ENV['FIRST_RESULTS'] || '').split(',') & [source.name, source.scraper, 'All']).any?
+  end
+
+  def prefill_results(source, url, results, ext = '.json')
+    puts "Writing the following json to spec/fakeweb/results/#{fakewebize(url)}#{ext} for #{source.name}:"
+    ap results
+    File.open(Rails.root.join("spec/fakeweb/results/#{fakewebize(url)}#{ext}"), 'w') { |f| f.puts results.to_json }
+  end
+
+
+  def fake_extract_show_urls_pages
+    # Some scrapers call read_url for extract_show_urls
+    Source.where("sources.scraper IN ('SmhScraper', 'TenScraper', 'TenMicroSiteScraper')").each do |source|
+      download_page_if_new(source, source.url)
+
+      fake_page(source.scraper.constantize, source.url)
+    end
+  end
+
+  def fake_page(scraper, url)
+    scraper.should_receive(:read_url).with(url).and_return(File.read(Rails.root + "spec/fakeweb/pages/#{fakewebize(url)}"))
+  end
+
+
+  def fakewebize(url)
+    url = url.gsub(%r{/}, '^').gsub(%r{&token=[^&]+|&omitFields=[^&]+}, '')
+    url = (url !~ /(\.html?|\.xml)$/ ? url + '.htm' : url)
   end
 end
