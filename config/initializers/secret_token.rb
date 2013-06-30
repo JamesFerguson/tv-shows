35cbdf1be094d9c47087345b256329a05788d193
JamesFerguson <jim@the-ocean.home.gateway>
2013-06-30 15:26:06 +1000

    Move secret token to env setting

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TvShows::Application.config.secret_token = '8a558064f411ee13ff41ba046f07666c1aa258187fc87d925b552ebd059e95bb18d1961288ebc873c40186f52e43ef7776f70f64ab5440d4ce0c7675593ee818'
+TvShows::Application.config.secret_token = ENV['SECRET_TOKEN']
